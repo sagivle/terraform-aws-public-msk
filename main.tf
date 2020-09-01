08ab8ece822fbdd7d6b03a52a5809f38c65fa3e6
sagiv levi <sagivl@gmail.com>
2020-09-01 09:35:44 +0300

    Update main.tf
    adding sort

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
-
 locals {
-  brokers = "${split("," , (replace(join(",", aws_msk_cluster.msk.*.bootstrap_brokers_tls), ":9094", "" )))}"
-  zookeeper = "${split("," , (replace(join(",", aws_msk_cluster.msk.*.zookeeper_connect_string), ":2181", "" )))}"
+  brokers = "${sort(split("," , (replace(join(",", aws_msk_cluster.msk.*.bootstrap_brokers_tls), ":9094", "" ))))}"
+  zookeeper = "${sort(split("," , (replace(join(",", aws_msk_cluster.msk.*.zookeeper_connect_string), ":2181", "" ))))}"
 }
 
 //  zookeeper
 
 data "dns_a_record_set" "dns_record_zookeeper" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   host = "${local.zookeeper[count.index]}"
 }
 
 data "aws_network_interfaces" "network_interfaces_zookeeper" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   filter {
     name   = "private-ip-address"
     values = ["${element(flatten(data.dns_a_record_set.dns_record_zookeeper.*.addrs), count.index)}"]
@@ -20,7 +19,7 @@ data "aws_network_interfaces" "network_interfaces_zookeeper" {
 }
 
 resource "aws_eip" "eip_zookeeper" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   vpc                       = true
   network_interface         = "${element(flatten(data.aws_network_interfaces.network_interfaces_zookeeper.*.ids), count.index)}"
   associate_with_private_ip = "${element(flatten(data.dns_a_record_set.dns_record_zookeeper.*.addrs), count.index)}"
@@ -33,12 +32,12 @@ resource "aws_eip" "eip_zookeeper" {
 //  brokers
 
 data "dns_a_record_set" "dns_record_brokers" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   host = "${local.brokers[count.index]}"
 }
 
 data "aws_network_interfaces" "network_interfaces_brokers" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   filter {
     name   = "private-ip-address"
     values = ["${element(flatten(data.dns_a_record_set.dns_record_brokers.*.addrs), count.index)}"]
@@ -46,7 +45,7 @@ data "aws_network_interfaces" "network_interfaces_brokers" {
 }
 
 resource "aws_eip" "eip_brokers" {
-  count = "${aws_msk_cluster.msk.number_of_broker_nodes}"
+  count = "${var.module_enabled ? aws_msk_cluster.msk.number_of_broker_nodes : 0}"
   vpc                       = true
   network_interface         = "${element(flatten(data.aws_network_interfaces.network_interfaces_brokers.*.ids), count.index)}"
   associate_with_private_ip = "${element(flatten(data.dns_a_record_set.dns_record_brokers.*.addrs), count.index)}"
